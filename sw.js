ee57574d314e65c39556b21efa396bcf752be704
Balty1991 <contact.vanzo@gmail.com>
2026-05-05 14:09:35 +0300

    Bump cache after reverting failed visual theme

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // BetAnalyticsProV3 service worker - fresh network runtime
-const CACHE='ba-v3-runtime-20260505-visual1214';
+const CACHE='ba-v3-runtime-20260505-revert-visual1214';
 self.addEventListener('install',event=>{event.waitUntil(self.skipWaiting())});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
